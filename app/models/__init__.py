d338040a55333b3724a005988f0f9e289bce8559
Michaela Majorošová <michaela.majorosova@youforce.net>
2025-02-04 22:43:13 +0100

    create domain model employee

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -3,4 +3,3 @@ from .generated_questions import GeneratedQuestions
 from .domain.employee import Employee
 from .evaluation import Evaluation
 from .review import Review
-from .domain.employee import Base
--- app/models/domain/employee.py
+++ app/models/domain/employee.py
@@ -1,7 +1,5 @@
 from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import declarative_base
-
-Base = declarative_base()
+from app.utils import Base
 
 
 class Employee(Base):
