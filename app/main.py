1864b50936504dd5854fb2b95cb97e207ac62a2f
Michaela Majorošová <michaela.majorosova@youforce.net>
2025-04-06 20:32:12 +0200

    create websocket endpoint

--- app/main.py
+++ app/main.py
@@ -1,8 +1,11 @@
-from fastapi import FastAPI
-from app.drivers.rest.routers import llama_router, questions_router, review_router, user_router, manager_router, employee_router
+import json
+
+from fastapi import FastAPI, WebSocket
+from app.drivers.rest.routers import llama_router, questions_router, review_router, user_router, manager_router, \
+    employee_router
 from app.adapters import global_exception_handler
 from fastapi.middleware.cors import CORSMiddleware
-
+from app.drivers.llama.llama_client import LlamaClient
 
 app = FastAPI(
     title="Employee Review Generator",
@@ -33,3 +36,59 @@ app.include_router(llama_router)
 app.include_router(review_router)
 app.include_router(manager_router)
 app.include_router(employee_router)
+
+ollamaClient = LlamaClient()
+
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    while True:
+        raw_text = await websocket.receive_text()
+
+        print(f"Received message: {raw_text}")
+
+        if not raw_text:
+            await websocket.send_text("Please write something.")
+            continue
+
+        try:
+            messages = json.loads(raw_text)
+
+            if not isinstance(messages, list):
+                await websocket.send_text("Invalid format. Expected a list of messages.")
+                continue
+
+            full_messages = [
+                {
+                    "role": "system",
+                    "content": "You are an AI assistant, which is helping me change the following review based on the "
+                               "user input."
+                }
+            ] + messages
+
+            data_from_llama = ollamaClient.client.chat(
+                model=ollamaClient.model,
+                messages=full_messages,
+                stream=True,
+                options={
+                    "temperature": 0.5,
+                    "top_p": 0.9,
+                    "top_k": 75
+                },
+                format={
+                    "type": "object",
+                    "properties": {
+                        "review": {
+                            "type": "string"
+                        }
+                    },
+                    "required": ["review"]
+                }
+            )
+
+            for chunk in data_from_llama:
+                await websocket.send_text(chunk["message"]["content"])
+
+        except json.JSONDecodeError:
+            await websocket.send_text("Invalid JSON. Please send a valid list of messages.")
